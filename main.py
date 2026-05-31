ae1ce247b4adc5ab02ccc948535afcb9c5fe6207
Arua Mabel Chinasa <nasaadanna@gmail.com>
2026-05-31 03:20:29 -0700

    Added user login

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi.responses import FileResponse
+import bcrypt
 from fastapi import FastAPI, Depends, HTTPException, status, File, UploadFile
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
@@ -31,7 +32,22 @@ app = FastAPI()
 # 3. Security Scheme
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-# --- YOUR @app.post ROUTES SHOULD BE RIGHT BELOW THIS ---
+
+
+
+
+# (And just in case you need the verify function to match it!)
+def verify_password(plain_password: str, hashed_password: str):
+    password_byte_enc = plain_password.encode('utf-8')
+    hashed_password_bytes = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(password=password_byte_enc, hashed_password=hashed_password_bytes)
+
+def get_password_hash(password: str):
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password=pwd_bytes, salt=salt)
+    return hashed_password.decode('utf-8')
+
 
 @app.get("/")
 def serve_frontend_root():
