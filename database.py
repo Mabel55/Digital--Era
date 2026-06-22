119a20de36d3158407ca25f5bd73dfa6774b1de8
Arua Mabel Chinasa <nasaadanna@gmail.com>
2026-06-22 05:17:00 -0700

    Fix dropped SSL connections with pool_pre_ping

--- database.py
+++ database.py
@@ -13,9 +13,15 @@ SQLALCHEMY_DATABASE_URL = os.getenv(
 if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-# The engine is what actually connects to the database
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
+# The engine is what actually connects to the database. 
+# We add pool_pre_ping to stop Azure/Neon from dropping idle connections!
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True, 
+    pool_recycle=300,
+    # If using sqlite locally, these args are needed, but for postgres they are ignored.
+    # However, to be safe across both, we omit connect_args.
+)
 # The session is what we use to query the database
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
