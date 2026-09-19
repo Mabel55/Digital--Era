a6a22b1ab52f0c18bc4dd4f7f1c9e39b6ddaeeb9
Arua Mabel Chinasa <nasaadanna@gmail.com>
2026-09-19 03:48:55 -0700

    Add 5 beginner conceptual lessons to Backend track and bump version to v52

--- frontend/src/hooks/useCurriculum.js
+++ frontend/src/hooks/useCurriculum.js
@@ -3,7 +3,7 @@ import { cacheCurriculum, getCachedCurriculum } from '../lib/offlineDB';
 
 // Bump this version string when curriculum.json is updated in a new deployment.
 // This allows the browser to cache the file aggressively between deploys.
-const CURRICULUM_VERSION = '4';
+const CURRICULUM_VERSION = '5';
 
 const fetchCurriculum = async () => {
   try {
